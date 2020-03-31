e1b074d619b968dd1f39975278e5d432e0484388
Lewis Bails <Lewis.Bails@infomedia.dk>
2020-03-31 08:43:08 +0200

    oops

--- util/matching.py
+++ util/matching.py
@@ -124,19 +124,19 @@ class CEM:
                               'vc': data_.loc[weights_ > 0, treatment].value_counts()}
             return results
         else:
-            weights_ = CEM.match(data_.drop(outcome, axis=1, treatment, bins)
+            weights_ = CEM.match(data_.drop(outcome, axis=1), treatment, bins)
             return CEM._regress_matched(data_, outcome, weights_, drop)
 
     @staticmethod
     def _regress_matched(data, outcome, weights, drop):
-        weights_=weights[weights > 0]
-        data_=data.loc[weights_.index, :].copy()
+        weights_ = weights[weights > 0]
+        data_ = data.loc[weights_.index, :].copy()
 
-        right=' + '.join(data_.drop([outcome] + drop, axis=1).columns.values)
-        formula=f'{outcome} ~ {right}'
-        glm=smf.glm(formula, data=data_, family=sm.families.Binomial(),
+        right = ' + '.join(data_.drop([outcome] + drop, axis=1).columns.values)
+        formula = f'{outcome} ~ {right}'
+        glm = smf.glm(formula, data=data_, family=sm.families.Binomial(),
                       var_weights=weights_)
-        result=glm.fit(method='bfgs')
+        result = glm.fit(method='bfgs')
         return result.summary()
 
     @staticmethod
@@ -151,7 +151,7 @@ class CEM:
     @staticmethod
     def coarsen(data, bins):
         ''' Coarsen data based on schema '''
-        df_coarse=data.apply(lambda x: CEM._cut(
+        df_coarse = data.apply(lambda x: CEM._cut(
             x, bins[x.name]['cut'], bins[x.name]['bins']) if x.name in bins else x, axis=0)
         return df_coarse
 
@@ -160,20 +160,20 @@ class CEM:
         ''' Evaluate histogram similarity '''
         from itertools import combinations
         from collections import OrderedDict
-        groups=data.groupby(treatment).groups
-        data_=data.drop(treatment, axis=1).copy()
-        cont_bins=4
-        bins=[min(len(x.unique()), cont_bins) for _, x in data_.items()]
-        ranges=[(x.min(), x.max()) for _, x in data_.items()]
+        groups = data.groupby(treatment).groups
+        data_ = data.drop(treatment, axis=1).copy()
+        cont_bins = 4
+        bins = [min(len(x.unique()), cont_bins) for _, x in data_.items()]
+        ranges = [(x.min(), x.max()) for _, x in data_.items()]
         try:
-            h={}
+            h = {}
             for k, i in groups.items():
-                h[k]=np.histogramdd(data_.loc[i, :].to_numpy(), bins=bins, range=ranges, density=False)[0]
-            L1={}
+                h[k] = np.histogramdd(data_.loc[i, :].to_numpy(), bins=bins, range=ranges, density=False)[0]
+            L1 = {}
             for pair in map(dict, combinations(h.items(), 2)):
-                pair=OrderedDict(pair)
-                (k_left, k_right), (h_left, h_right)=pair.keys(), pair.values()  # 2 keys 2 histograms
-                L1[tuple([k_left, k_right])]=np.sum(
+                pair = OrderedDict(pair)
+                (k_left, k_right), (h_left, h_right) = pair.keys(), pair.values()  # 2 keys 2 histograms
+                L1[tuple([k_left, k_right])] = np.sum(
                     np.abs(h_left / len(groups[k_left]) - h_right / len(groups[k_right]))) / 2
         except Exception as e:
             print(e)
@@ -187,61 +187,61 @@ class CEM:
         from scipy.stats import ttest_ind_from_stats
         from functools import reduce
         from collections import OrderedDict
-        df2=pd.concat((self.data, self.weights.rename('weights')), axis=1)
-        df2=df2.loc[df2['weights'] > 0, :]
-        res=OrderedDict()
+        df2 = pd.concat((self.data, self.weights.rename('weights')), axis=1)
+        df2 = df2.loc[df2['weights'] > 0, :]
+        res = OrderedDict()
         for i, g in df2.groupby(self.treatment):
-            weight=g['weights'].sum()
+            weight = g['weights'].sum()
 
-            WSOUT=(g[self.outcome] * g['weights']).sum()
-            wave=WSOUT / weight
+            WSOUT = (g[self.outcome] * g['weights']).sum()
+            wave = WSOUT / weight
 
-            ave=g[self.outcome].mean()
-            WSSR=(g['weights'] * (g[self.outcome] - ave)**2).sum()
-            wstd=np.sqrt(WSSR / weight)
+            ave = g[self.outcome].mean()
+            WSSR = (g['weights'] * (g[self.outcome] - ave)**2).sum()
+            wstd = np.sqrt(WSSR / weight)
 
-            res[i]=[wave, wstd, len(g)]
+            res[i] = [wave, wstd, len(g)]
 
         return res, ttest_ind_from_stats(*list(reduce(lambda x, y: x + y, res.values())), equal_var=False)
 
 
 def summary_to_frame(summary, n_bins, vc, statistic, dtype=None):
-    observations=pd.read_html(summary.tables[0].as_html())[0].iloc[0, 3]
-    results=pd.read_html(summary.tables[1].as_html())[0]
-    results.iloc[0, 0]='covariate'
-    results.columns=results.iloc[0]
-    results=results[1:]
-    results['observations']=observations
-    results['n_bins']=n_bins
-    results['controls']=vc[False]
-    results['treatments']=vc[True]
-    results['coef']=results['coef'].astype(float)
-    results['P>|z|']=results['P>|z|'].astype(float)
-    results['statistic']=statistic
+    observations = pd.read_html(summary.tables[0].as_html())[0].iloc[0, 3]
+    results = pd.read_html(summary.tables[1].as_html())[0]
+    results.iloc[0, 0] = 'covariate'
+    results.columns = results.iloc[0]
+    results = results[1:]
+    results['observations'] = observations
+    results['n_bins'] = n_bins
+    results['controls'] = vc[False]
+    results['treatments'] = vc[True]
+    results['coef'] = results['coef'].astype(float)
+    results['P>|z|'] = results['P>|z|'].astype(float)
+    results['statistic'] = statistic
     if dtype:
         for c in results.columns:
             if results[c].dtype not in (object, bool):
-                results[c]=results[c].astype(dtype)
+                results[c] = results[c].astype(dtype)
     return results
 
 
 def mahalanobis_frontier(data, on):
     print('Distances..', end=' ')
-    distances=get_distances(data, 'mahalanobis', on)
-    MMD_tc=distances.min(axis=0)  # gives closest control to each treatment
-    MMD_ct=distances.min(axis=1)  # gives closest treatment to each control
-    MMD=pd.concat([MMD_tc, MMD_ct])
+    distances = get_distances(data, 'mahalanobis', on)
+    MMD_tc = distances.min(axis=0)  # gives closest control to each treatment
+    MMD_ct = distances.min(axis=1)  # gives closest treatment to each control
+    MMD = pd.concat([MMD_tc, MMD_ct])
     print('ready.')
 
-    pruned_controls=[]
-    pruned_treatments=[]
-    AMD_=[]
-    radii=[]
-    unique_distances=sorted(set(np.ravel(distances.round(1).to_numpy())), reverse=True)
+    pruned_controls = []
+    pruned_treatments = []
+    AMD_ = []
+    radii = []
+    unique_distances = sorted(set(np.ravel(distances.round(1).to_numpy())), reverse=True)
     for radius in tqdm(unique_distances):
-        AMD=MMD[MMD <= radius].mean()  # average min distance below threshold
+        AMD = MMD[MMD <= radius].mean()  # average min distance below threshold
         AMD_.append(AMD)
-        treatments, controls=prune_by_distance(distances, radius)  # controls that werent pruned
+        treatments, controls = prune_by_distance(distances, radius)  # controls that werent pruned
         pruned_controls.append(len(distances) - len(controls))
         pruned_treatments.append(len(distances.columns) - len(treatments))
         radii.append(radius)
@@ -253,20 +253,20 @@ def mahalanobis_frontier(data, on):
     return pd.DataFrame.from_dict({'pruned controls': pruned_controls, 'pruned treatments': pruned_treatments, 'AMD': AMD_, 'radius': radii}, orient='columns')
 
 
-def prune_by_distance(distances, caliper: Union[int, float]=5):
-    matches=distances.le(caliper)
+def prune_by_distance(distances, caliper: Union[int, float] = 5):
+    matches = distances.le(caliper)
     # controls that have a treatment match
-    control_idx=[idx for idx in matches.index if matches.loc[idx, :].sum()]
+    control_idx = [idx for idx in matches.index if matches.loc[idx, :].sum()]
     # treatments that have a control match
-    treatment_idx=[idx for idx in matches.columns if matches[idx].sum()]
+    treatment_idx = [idx for idx in matches.columns if matches[idx].sum()]
     return treatment_idx, control_idx
 
 
 def get_distances(data, distance, t, dtype=float):
-    treatment, control=list(map(lambda x: x.index, split(data, t)))
-    distances=pdist(data.drop(t, axis=1).astype(dtype), distance)
-    distances=pd.DataFrame._from_arrays(squareform(distances), index=data.index, columns=data.index.values)
-    distances=distances.loc[control, treatment.values]
+    treatment, control = list(map(lambda x: x.index, split(data, t)))
+    distances = pdist(data.drop(t, axis=1).astype(dtype), distance)
+    distances = pd.DataFrame._from_arrays(squareform(distances), index=data.index, columns=data.index.values)
+    distances = distances.loc[control, treatment.values]
     distances.index.rename('control', inplace=True)
     distances.columns.rename('treatment', inplace=True)
     return distances
@@ -274,16 +274,16 @@ def get_distances(data, distance, t, dtype=float):
 
 def match_by_distance(data, on, dv, distance, caliper):
     if caliper == 'auto':
-        df=mahalanobis_frontier(data.drop(dv, axis=1), on)
-        df['AMD']=(df['AMD'] - df['AMD'].min()) / (df['AMD'].max() - df['AMD'].min())
-        df['pruned controls']=(df['pruned controls'] - df['pruned controls'].min()) / \
+        df = mahalanobis_frontier(data.drop(dv, axis=1), on)
+        df['AMD'] = (df['AMD'] - df['AMD'].min()) / (df['AMD'].max() - df['AMD'].min())
+        df['pruned controls'] = (df['pruned controls'] - df['pruned controls'].min()) / \
             (df['pruned controls'].max() - df['pruned controls'].min())
-        df['dto']=np.sqrt(df['AMD']**2 + df['pruned controls']**2)
-        caliper=df.loc[df['dto'] == df['dto'].min(), 'radius']
+        df['dto'] = np.sqrt(df['AMD']**2 + df['pruned controls']**2)
+        caliper = df.loc[df['dto'] == df['dto'].min(), 'radius']
 
-    distances=get_distances(data.drop(dv, axis=1), distance, on)
-    treatments, controls=prune_by_distance(distances, caliper)
-    df_matched=data.loc[controls + treatments, :]
+    distances = get_distances(data.drop(dv, axis=1), distance, on)
+    treatments, controls = prune_by_distance(distances, caliper)
+    df_matched = data.loc[controls + treatments, :]
     return df_matched
 
 
@@ -292,11 +292,11 @@ def split(data, on):
 
 
 def covariate_dists(data, on, kde=True, hist=True, n_bins=10):
-    vals=data[on].unique()
-    flatui=["#2ecc71", "#9b59b6", "#3498db", "#e74c3c", "#34495e"]
+    vals = data[on].unique()
+    flatui = ["#2ecc71", "#9b59b6", "#3498db", "#e74c3c", "#34495e"]
 
     for col in data.drop(on, axis=1).columns:
-        bins=np.linspace(data[col].min(), data[col].max(), n_bins) if hist else None
+        bins = np.linspace(data[col].min(), data[col].max(), n_bins) if hist else None
         try:
             for i, val in enumerate(vals):
                 sns.distplot(data[data[on] == val][col],
@@ -314,12 +314,12 @@ def covariate_dists(data, on, kde=True, hist=True, n_bins=10):
 
 
 def dist_test(data, on, func, **kwargs):
-    v=data[on].unique()
+    v = data[on].unique()
     if len(v) != 2:
         print('Dichotamize variable of interest and try again.')
         return None
 
-    res=[]
+    res = []
     for col in data.drop(on, axis=1).columns:
         if len(data[col].unique()) <= 2:
             res.append([np.nan])
@@ -332,22 +332,22 @@ def dist_test(data, on, func, **kwargs):
 def binom_z_tests(data, on):
     from scipy.stats import norm
 
-    v=data[on].unique()
+    v = data[on].unique()
     if len(v) != 2:
         print(f'Dichotamize variable of interest and try again. {len(v)}')
         return None
 
-    res=[]
+    res = []
     for col in data.drop(on, axis=1).columns:
         if len(data[col].unique()) != 2:
             res.append([np.nan])
         else:
-            p1=data[data[on] == v[0]][col].mean()
-            p2=data[data[on] == v[1]][col].mean()
-            n1=len(data[data[on] == v[0]][col])
-            n2=len(data[data[on] == v[1]][col])
-            p=(n1 * p1 + n2 * p2) / (n1 + n2)
-            z=(p1 - p2) / np.sqrt(p * (1 - p) * (1 / n1 + 1 / n2))
+            p1 = data[data[on] == v[0]][col].mean()
+            p2 = data[data[on] == v[1]][col].mean()
+            n1 = len(data[data[on] == v[0]][col])
+            n2 = len(data[data[on] == v[1]][col])
+            p = (n1 * p1 + n2 * p2) / (n1 + n2)
+            z = (p1 - p2) / np.sqrt(p * (1 - p) * (1 / n1 + 1 / n2))
             res.append([z, norm.sf(abs(z)) * 2])
 
     return pd.DataFrame.from_records(res, index=data.drop(on, axis=1).columns, columns=['statistic'])
