dca9f8796e33d13c95b0c4e8d966b2bdf1161a0d
Lewis.Bails <Lewis.Bails@infomedia.dk>
2020-04-22 16:46:23 +0200

    Clean up analysis

--- util/evaluation.py
+++ util/evaluation.py
@@ -7,6 +7,85 @@ from functools import reduce
 from scipy.stats import norm
 
 
+def to_frame(summary: 'statsmodels.Summary') -> pd.DataFrame:
+    '''Convert Summary object to DataFrame of covariate regression statistics'''
+    pd_summary = pd.read_html(summary.tables[1].as_html())[0]
+    pd_summary.iloc[0, 0] = 'covariate'
+    pd_summary.columns = pd_summary.iloc[0]
+    pd_summary = pd_summary[1:]
+    pd_summary.set_index('covariate', inplace=True)
+    return pd_summary.astype(float)
+
+
+def residuals(group, sd=2):
+    avg_pred = group['pred'].mean()
+    avg_resid = group['residual'].mean()
+    bound = sd * np.sqrt(avg_pred * (1 - avg_pred) / len(group))
+    df = pd.DataFrame.from_dict({'pred': [avg_pred], 'residual': [avg_resid], 'bound': [bound], 'count': [
+                                len(group)], 'inside': (avg_resid <= bound) and (avg_resid >= -bound)}, orient='columns')
+    for c in continuous:
+        try:
+            df[c] = group[c].mean()
+        except Exception as e:
+            print(e)
+            df[c] = np.nan
+    return df
+
+
+def binned_residuals(estimator, X, y, bins=None, on='pred'):
+    y_pred = estimator.predict(X)
+    y_ = pd.DataFrame.from_dict({'pred': y_pred, 'true': y, 'residual': y - y_pred}, orient='columns')
+    for c in X.columns:
+        y_[c] = X[c]
+    if bins is None:
+        bins = int(np.sqrt(len(y_)))
+    print(f'Using {bins} bins.')
+    y_['bin'] = pd.qcut(y_[on], q=bins, duplicates='drop')
+    y_.reset_index(drop=True, inplace=True)
+    y_resid = y_.groupby('bin').apply(residuals)
+    y_resid.index = y_resid.index.droplevel(-1)
+    return y_resid
+
+
+def plot_binned_residuals(summary, lims=None, on='pred'):
+    summary['lower'] = -summary['bound']
+    summary['upper'] = summary['bound']
+    cmap = {True: 'g', False: 'r'}
+    ax = sns.scatterplot(x=on, y='residual', data=summary, hue='inside',
+                         style='inside', palette=cmap, legend=False)
+    ax = sns.lineplot(x=on, y='lower', data=summary, ax=ax, c='b')
+    ax = sns.lineplot(x=on, y='upper', data=summary, ax=ax, c='b')
+
+    if lims is not None:
+        plt.xlim(lims[0], lims[1])
+    plt.ylabel('Average residual')
+    plt.xlabel(f'Average {on}')
+    plt.title(f'Binned residuals for {on}')
+    return ax
+
+
+def odds_ratios(g, treatment, hue='distance', hue_values=35, p=0.1, plot=True):
+    rows = []
+    if isinstance(hue_values, int):
+        vals = [hue_values]
+    else:
+        vals = hue_values
+    for v in vals:
+        base = odds(g, p, **{treatment: False, hue: v})
+        odds_Xi = odds(g, p, **{treatment: True, hue: v})
+        OR = odds_Xi / base
+        row = {'OR': OR, f'{hue}': v}
+        rows.append(row)
+    res = pd.DataFrame.from_records(rows)
+    if plot:
+        ax = sns.lineplot(x=hue, y='OR', data=res)
+        ax.set_xlabel(hue)
+        ax.set_ylabel('Odds Ratio')
+        ax.set_title(f'Odds ratios for {treatment} at varying {hue}')
+        return res, ax
+    return res
+
+
 def odds(data, p=1, **kwargs):
     ''' Added odds thanks to these coefficients and theyre values '''
     kwargs.update({'np': np})
